319dc24d39a52b07b1fbb6fe32b67b71c18669d5
haita <haita@192.168.40.234>
2019-12-05 19:03:05 +0200

    test 1

--- PantherSnakeGame/src/Controller/Main.java
+++ PantherSnakeGame/src/Controller/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new MainScreen(1);
-		System.out.println("welcome");
+		System.out.println("welcome ");
 		
 		
 	}
