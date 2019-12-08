1731398c4b720821e81006be01de84f6db00ec7e
wasimzzz <38220773+wasimzzz@users.noreply.github.com>
2019-12-08 17:10:31 +0200

    Made by Ansam

---
+++ PantherSnakeGame/src/recources/Constants.java
@@ -0,0 +1,15 @@
+package recources;
+
+/**
+ * 
+ * class to save Constants in the project
+ *
+ */
+public class Constants {
+	
+	/* files name */
+	public final static  String QUESTIONFILENAME="src\\resources\\questions.json";
+	
+	
+
+}
