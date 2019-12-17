0d9b05d77f67234e5a96a59faca46c18a59783ab
maha <31921776+mahaRaef@users.noreply.github.com>
2019-12-18 00:16:35 +0200

    update view

---
+++ PantherSnakeGame/src/View/Instructions.java
@@ -0,0 +1,56 @@
+package View;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+
+public class Instructions {
+
+	public JFrame Instructionsframe;
+
+	/**
+	 * Create the application.
+	 */
+	public Instructions() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		JFrame Instructions = new JFrame();
+		Instructions.setIconImage(Toolkit.getDefaultToolkit().getImage(Instructions.class.getResource("/images/instructionss.png")));
+		Instructions.setTitle("Instructions");
+		Instructionsframe = Instructions;
+		Instructions.getContentPane().setLayout(null);
+		
+		JButton btnBack = new JButton("");
+		btnBack.setBackground(Color.WHITE);
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				MainScreen f = new MainScreen();
+				f.framefirst.setVisible(true);
+				Instructionsframe.dispose();	
+				
+			}
+		});
+		btnBack.setIcon(new ImageIcon(Instructions.class.getResource("/images/back.jpg")));
+		btnBack.setBounds(15, 460, 107, 45);
+		Instructions.getContentPane().add(btnBack);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Instructions.class.getResource("/images/unnamed.jpg")));
+		lblNewLabel.setBounds(0, -7, 909, 528);
+		Instructions.getContentPane().add(lblNewLabel);
+		Instructionsframe.setBounds(100, 100, 931, 577);
+		Instructionsframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
---
+++ PantherSnakeGame/src/View/Setting.java
@@ -0,0 +1,89 @@
+package View;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JCheckBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Setting {
+
+	public JFrame Settingframe;
+
+
+
+	/**
+	 * Create the application.
+	 */
+	public Setting() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		Settingframe = new JFrame();
+		Settingframe.setBackground(Color.WHITE);
+		Settingframe.setBounds(100, 100, 631, 513);
+		Settingframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Settingframe.getContentPane().setLayout(null);
+		
+		JLabel lblSettings = new JLabel("Settings");
+		lblSettings.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSettings.setFont(new Font("Segoe Print", Font.BOLD, 30));
+		lblSettings.setBounds(247, 119, 165, 53);
+		Settingframe.getContentPane().add(lblSettings);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon(Setting.class.getResource("/images/SnakeeB.png")));
+		label_1.setBounds(179, 67, 92, 83);
+		Settingframe.getContentPane().add(label_1);
+		
+		JButton button = new JButton("");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				MainScreen f = new MainScreen();
+				f.framefirst.setVisible(true);
+				Settingframe.dispose();	
+				
+			}
+		});
+		button.setBackground(Color.WHITE);
+		button.setIcon(new ImageIcon(Setting.class.getResource("/images/esc1.jpg")));
+		button.setBounds(15, 399, 53, 43);
+		Settingframe.getContentPane().add(button);
+		
+		JCheckBox chckbxOnoff = new JCheckBox("On/Off");
+		chckbxOnoff.setFont(new Font("Segoe Print", Font.PLAIN, 19));
+		chckbxOnoff.setBounds(362, 200, 139, 29);
+		Settingframe.getContentPane().add(chckbxOnoff);
+		
+		JCheckBox checkBox = new JCheckBox("On/Off");
+		checkBox.setFont(new Font("Segoe Print", Font.PLAIN, 19));
+		checkBox.setBounds(362, 270, 139, 29);
+		Settingframe.getContentPane().add(checkBox);
+		
+		JLabel label_2 = new JLabel("");
+		label_2.setIcon(new ImageIcon(Setting.class.getResource("/images/music.png")));
+		label_2.setBounds(179, 200, 42, 29);
+		Settingframe.getContentPane().add(label_2);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(Setting.class.getResource("/images/bubble.png")));
+		label.setBounds(-27, 0, 659, 509);
+		Settingframe.getContentPane().add(label);
+		
+		JLabel label_3 = new JLabel("");
+		label_3.setIcon(new ImageIcon(Setting.class.getResource("/images/sound.png")));
+		label_3.setBounds(179, 274, 69, 20);
+		Settingframe.getContentPane().add(label_3);
+	}
+}
